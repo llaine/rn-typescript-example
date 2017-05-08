23bdf33371ad3e74fdb23b248fe0bc892045758d
Christian Brevik <christian.brevik@gmail.com>
2017-05-08 12:37:07 +0200

    Update webpack config to resolve tsx extensions

--- webpack.haul.js
+++ webpack.haul.js
@@ -1,13 +1,17 @@
-module.exports = ({ platform }, { module }) => ({
+module.exports = ({ platform }, { module, resolve }) => ({
   entry: `./src/index.${platform}.tsx`,
   module: {
     ...module,
     rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader'
-        },
-        ...module.rules
-      ]
-  }
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      ...module.rules
+    ]
+  },
+  resolve: {
+    ...resolve,
+    extensions: ['.ts', '.tsx', ...resolve.extensions],
+  },
 });
